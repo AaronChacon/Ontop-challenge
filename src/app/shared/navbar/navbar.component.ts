39c6ac5dcf3c0694f3087b7ff714cddb9aea8d2e
Aaron Chacon <aaronch.dev@gmail.com>
2021-10-29 18:59:01 -0400

    feat: navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'ontop-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+
+  logo:string = 'assets/img/default/logo.png';
+  arrow_down:string = '../../../assets/img/icons/arrow_down.svg';
+  userImg:string = '../../../assets/img/default/user.svg';
+
+  menuInfo:any = [
+    {
+      icon: '../../../assets/img/icons/user.svg',
+      name:'Contracts'
+    },
+    {
+      icon: '../../../assets/img/icons/document.svg',
+      name:'Documents'
+    },
+    {
+      icon: '../../../assets/img/icons/pay.svg',
+      name:'Payout'
+    },
+    {
+      icon: '../../../assets/img/icons/calendar.svg',
+      name:'Time'
+    }
+  ]
+  userSelect:any = [
+  ]
 
   constructor() { }
 
