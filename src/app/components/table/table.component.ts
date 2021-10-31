736969a9ef48224783b655e21b53b213da1c6226
Aaron Chacon <aaronch.dev@gmail.com>
2021-10-31 00:42:08 -0400

    fix: clear files and ts

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,39 +1,6 @@
 import {Component, Input} from '@angular/core';
 import { Contractor } from 'src/app/core/interfaces/basic.interfaces';
 
-
-
-/* const ELEMENT_DATA: Contractor[] = [
-  {
-    id: 1,
-    contractorName: 'Darlene Robertson',
-    imgUrl: 'assets/img/default/user.png',
-    type: 'Traditional',
-    startDate: 'Mar 4, 2021',
-    amount: 200,
-    status: 'active',
-  },
-  {
-    id: 2,
-    contractorName: 'Darlene Robertson',
-    imgUrl: 'assets/img/default/user1.png',
-    type: 'Traditional',
-    startDate: 'Mar 4, 2021',
-    amount: 200,
-    status: 'signature pending',
-  },
-  {
-    id: 3,
-    contractorName: 'Darlene Robertson',
-    imgUrl: 'assets/img/default/user2.png',
-    type: 'Traditional',
-    startDate: 'Mar 4, 2021',
-    amount: 1200,
-    status: 'active',
-  },
-]; */
-
-
 @Component({
   selector: 'ontop-table',
   templateUrl: './table.component.html',
